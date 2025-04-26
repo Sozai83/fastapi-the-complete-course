cfa98e900b5341a639164cd59e739f5d9e0be7e9
Souzai83 <chikushi3.227@gmail.com>
2025-04-26 17:13:33 +1000

    added encapsulation - lesson 46

--- OOP_Practice/Enemy.py
+++ OOP_Practice/Enemy.py
@@ -2,15 +2,24 @@ class Enemy:
     ## Constructor for the Enemy class when initializing an enemy object.
     ## Self referts to the instance of the class itself.
     def __init__(self, type_of_enemy, health = 10, attack_damage = 1):
-        self.type_of_enemy: str = type_of_enemy
+        self.__type_of_enemy: str = type_of_enemy
         self.health_points: int = health
         self.attack_damage: int = attack_damage
 
+    def get_type_of_enemy(self):
+        return self.__type_of_enemy
+    
+    """"
+    As I don't want to change the type of enemy after it has been created, I will not use this setter method.
+    def set_type_of_enemy(self, type_of_enemy):
+        self.__type_of_enemy = type_of_enemy
+    """
+
     def talk(self):
-        print(f"I am a {self.type_of_enemy}. Be prepared to fight!")
+        print(f"I am a {self.get_type_of_enemy()}. Be prepared to fight!")
 
     def walk_forward(self):
-        print(f"{self.type_of_enemy} moves closer to you!")
+        print(f"{self.get_type_of_enemy()} moves closer to you!")
 
     def attack(self):
-        print(f"{self.type_of_enemy} attacks you for {self.attack_damage} damage!")
+        print(f"{self.get_type_of_enemy()} attacks you for {self.attack_damage} damage!")
--- OOP_Practice/main.py
+++ OOP_Practice/main.py
@@ -2,6 +2,8 @@ from Enemy import *
 
 enemy = Enemy("Goblin", 20, 3)
 
+print(enemy.get_type_of_enemy())
+
 enemy.talk()
 enemy.walk_forward()
 enemy.attack()
